aa20d2ce7da1d6e644a287e2a29dd50e8cb3dee6
Fishniki <nikichum96@gmail.com>
2025-09-15 15:18:54 +0700

    first commit

---
+++ helper/helo_worlrd_test.go
@@ -0,0 +1,80 @@
+package helper
+
+import (
+	"fmt"
+	"runtime"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+
+func TestMain(m *testing.M) {
+	// Seperti Layout Pada Component
+	// Test ini akan di jalankan pertama kali
+	// Dan akan di akhiri setelah semua test sudah di jalankan
+	fmt.Println("Sebelum unit test")
+	m.Run()
+	fmt.Println("Setelah unit test")
+}
+
+func TestSubTest(t *testing.T) {
+	t.Run("Niki", func(t *testing.T) {
+		result := HelloWorld("Niki")
+		require.Equal(t, "Hello Niki", result, "Harusnya menampilkan Niki")
+	})
+	t.Run("Hamdani", func(t *testing.T) {
+		result := HelloWorld("Hamdani")
+		require.Equal(t, "Hello Hamdani", result, "Harusnya menampilkan Niki")
+	})
+
+	//jika hanya ingin menjalankan subtest nyasaja
+	//go test -v -run=TestSubTest/NamaSubtest
+}
+
+func TestSkip(t *testing.T) {
+	if runtime.GOOS == "windows"{
+		t.Skip("Can not run on Windows OSbu6j7v")
+	}
+	result := HelloWorld("Niki")
+	assert.Equal(t, "Hello Niki", result, "Harusnya menampilkan Hello Niki")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+
+func TestHelloWordAssert(t *testing.T) {
+	result := HelloWorld("Niki")
+	//assert akan menjalankan code di bawahnya
+	assert.Equal(t, "Hello Niki", result, "Harusnya menampilkan Hello Niki")
+	fmt.Println("TestHelloWorld with Assert Done")
+}
+
+func TestHelloWordRequire(t *testing.T) {
+	result := HelloWorld("Niki")
+	//require akan langsung berhenti dan perintah di bawahnya tidak akan di jalankan
+	require.Equal(t, "Hello Niki", result, "Harusnya menampilkan Hello Niki")
+	fmt.Println("TestHelloWorld with Require Done")
+}
+
+
+//Tanpa package stretchr/testify/assert
+// func TestHelloWorld(t *testing.T) {
+// 	result := HelloWorld("Niki")
+// 	if result != "Hello Niki" {
+// 		//t.Fail() //Kalo fail akan tetap menjalangkan kode di bawahnya
+// 		t.Fatal("Harusnya menampilkan Hi Niki")
+// 	}
+// 	fmt.Println("Test HelloWorld")
+// }
+
+// func TestHelloWorldHamdani(t *testing.T) {
+// 	result := HelloWorld("Hamdani")
+// 	if result != "Hello Hamdani" {
+// 		//t.FailNow() //failnow akan langsung memberhentikan code, dan kode di bawahnya tidak akan di jalankan
+// 		t.Fatal("Harusnya menampiplkan Hi Hamdani")
+// 	}
+
+// 	fmt.Println("Test HelloWordHamdani")
+// }
+//End tanpa package stretchr/testify/assert
